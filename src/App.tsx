1d4e9e536d5ea26fac0af9857398d268d3d95286
ZiarayZ <jakesmith@binaryrage.com>
2022-11-12 22:29:29 +0000

    Deleted Home path -There's a default path to use instead

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes";
+import { sitemap } from './sitemaps/sitemap';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path={sitemap.path} element={<Home />} />
       </Routes>
     </HashRouter >
   );
--- src/sitemaps/sitemap.ts
+++ src/sitemaps/sitemap.ts
@@ -1,7 +1,6 @@
 import { CreateParameters, RouteWithoutParams, RouteMap } from "./routemap";
 
 export const routes = new RouteMap({
-    Home: RouteWithoutParams,
     /* Payday2: new RouteMap({
         PerkDecks: RouteWithoutParams //TODO
     }) */
