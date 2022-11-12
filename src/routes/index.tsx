7e386bebea65b3f32cdc1c2c3ac44d3eafc436c6
ZiarayZ <jakesmith@binaryrage.com>
2022-11-12 22:53:28 +0000

    add redirecting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { redirect } from "react-router-dom";
+import { sitemap } from "../sitemaps/sitemap";
 import Payday2FC from "./Payday2";
 
 const Home: React.FC = () => {
@@ -8,6 +10,7 @@ const Home: React.FC = () => {
     return <>
         <h1>Home</h1>
         <p>Hello, World!</p>
+        <a onClick={() => redirect(sitemap.Payday2.Home.path)}>Payday 2</a>
     </>
 }
 
