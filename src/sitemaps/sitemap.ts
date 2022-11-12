57bb9671abe69d6c55acc382cf1233039f553ae7
ZiarayZ <jakesmith@binaryrage.com>
2022-11-12 22:28:06 +0000

    add TODO

--- src/sitemaps/sitemap.ts
+++ src/sitemaps/sitemap.ts
@@ -1,7 +1,10 @@
 import { CreateParameters, RouteWithoutParams, RouteMap } from "./routemap";
 
 export const routes = new RouteMap({
-    Home: RouteWithoutParams
+    Home: RouteWithoutParams,
+    /* Payday2: new RouteMap({
+        PerkDecks: RouteWithoutParams //TODO
+    }) */
 });
 
 export const sitemap = routes.Mount("/");
