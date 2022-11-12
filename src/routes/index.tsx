4104410949da3074eb806729450c14cbe5e6589c
ZiarayZ <jakesmith@binaryrage.com>
2022-11-12 21:58:48 +0000

    replace reactjs page with basic hello world

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+
+    //Redirects to different games ig?
+
+    return <>
+        <p>Hello, World!</p>
+    </>
+}
+
+export default Home;
