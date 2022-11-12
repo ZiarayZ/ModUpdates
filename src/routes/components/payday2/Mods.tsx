d27f0dc6005ad7de5e46095975d0ff86dc5a9f89
ZiarayZ <jakesmith@binaryrage.com>
2022-11-12 23:02:46 +0000

    New files into components to fix deploy

---
+++ src/routes/components/payday2/Mods.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Mods: React.FC = () => {
+
+    //Redirects to different games ig?
+
+    return <>
+        <h1>Mods</h1>
+        <p>Hello, World!</p>
+    </>
+}
+
+export default Mods;
---
+++ src/routes/components/payday2/PerkDecks.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PerkDecks: React.FC = () => {
+
+    //Redirects to different games ig?
+
+    return <>
+        <h1>Perk Decks</h1>
+        <p>Hello, World!</p>
+    </>
+}
+
+export default PerkDecks;
