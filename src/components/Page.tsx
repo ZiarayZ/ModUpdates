59f5773385d74adb3a5033c0f262f55a28ace227
ZiarayZ <jakesmith@binaryrage.com>
2022-11-13 16:27:49 +0000

    Page is now flex

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -4,10 +4,17 @@ import styled from "styled-components";
 import Navigation from "./Navigation";
 
 const StyledPage = styled.div`
-
+    background-color: ${props => props.theme.colors.pageBackground};
+    color: ${props => props.theme.colors.pageText};
+    display: flex;
+    flex-direction: column;
+    gap: ${props => props.theme.spacing.m};
+    height: 100%;
+    overflow: auto;
 `;
 const PageContent = styled.div`
-
+    flex: 1;
+    padding: ${props => props.theme.spacing.l};
 `;
 
 interface PageProps { }
