2b1aeaad78d28a28d62d0c1ae4e0346977697f02
ZiarayZ <jakesmith@binaryrage.com>
2022-11-15 23:05:21 +0000

    pass height down make sure nav is fixed

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,17 +10,20 @@ const StyledNav = styled.nav`
     position: sticky;
     top: 0;
     overflow: auto;
+    height: 50px;
 
     background-color: ${props => props.theme.colors.navBackground};
 `;
 const Banner = styled.div`
     display: flex;
+    height: inherit;
 `;
 const HeaderDiv = styled.div`
-    margin: auto;
+    height: inherit;
 `;
 const Nav = styled.div`
     flex: 1;
+    height: inherit;
 `;
 
 const Navigation: React.FC = () => {
